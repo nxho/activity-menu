699c6c6bffc981de513b7155a0b239f9b67088d7
Nathaniel Ho <nxho93@gmail.com>
2025-04-25 19:11:48 -0400

    add script

---
+++ push-activity-menu.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+src="/Users/nathanielho/Documents/personal"
+src_filename="Activity Menu.md"
+dst="/Users/nathanielho/Documents/activity-menu"
+dst_filename="index.md"
+
+# Exit if files are identical
+if cmp -s "$src/$src_filename" "$dst/$dst_filename"; then
+    echo "Files are identical. No changes needed."
+    exit 0
+fi
+
+cp "$src/$src_filename" "$dst/$dst_filename"
+cd "$dst"
+git commit -am 'Update activity menu'
+git push
+
